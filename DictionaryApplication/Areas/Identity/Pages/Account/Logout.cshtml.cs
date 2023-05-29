4d0337b066c35cc6fd30aaef0e1649f9de4e8a24
Bohdan Savastieiev <bohdan.savastieiev.it@gmail.com>
2023-05-29 22:35:42 +0300

    few additions

--- DictionaryApplication/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ DictionaryApplication/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -30,7 +30,7 @@ namespace DictionaryApplication.Areas.Identity.Pages.Account
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
-                return LocalRedirect(returnUrl);
+                return RedirectToPage("/Index");
             }
             else
             {
